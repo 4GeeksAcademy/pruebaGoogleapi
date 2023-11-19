9509ee0f5e077120bac47589a0aa2fb23ca3a925
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-11-19 02:35:11 +0000

    google map api

--- src/front/js/component/map.js
+++ src/front/js/component/map.js
@@ -1,11 +1,12 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript, StandaloneSearchBox  } from "@react-google-maps/api";
 import { useMemo } from "react";
 import  React, { useState } from "react";
 import "../../styles/map.css";
 
 export const Map = () => {
-  const { isLoaded } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.API_KEY,
+    libraries: ["places"], 
   });
   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
   const [map, setMap] = useState(null);
@@ -31,6 +32,27 @@ export const Map = () => {
     console.log(markerPosition)
   };
 
+  const onPlacesChanged = () => {
+    const places = searchBox.getPlaces();
+    if (places.length > 0) {
+      const place = places[0];
+      setMarkerPosition({
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      });
+      if (map) {
+        map.panTo({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() });
+      }
+      console.log(markerPosition);
+    }
+  };
+
+  let searchBox;
+  const onSearchBoxLoad = ref => {
+    searchBox = ref;
+  };
+
+
   return (
     <div className="App">
       {!isLoaded ? (
@@ -45,6 +67,31 @@ export const Map = () => {
           >
           <Marker position={markerPosition} icon={"https://maps.google.com/mapfiles/ms/icons/green-dot.png"} draggable={true}
             onDragEnd={onMarkerDragEnd} />
+
+          <StandaloneSearchBox
+            onLoad={onSearchBoxLoad}
+            onPlacesChanged={onPlacesChanged}
+          >
+            <input
+              type="text"
+              placeholder="Buscar dirección"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                left: "50%",
+                marginLeft: "-120px"
+              }}
+            />
+          </StandaloneSearchBox>
         </GoogleMap>
       )}
     </div>
